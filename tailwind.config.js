9364eb4e5eb07a3da2bbdfd02bc8560631bbc38b
samiullah58 <samiullahrashid4@gmail.com>
2024-02-20 15:02:37 +0500

    on vanilla

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Montserrat', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
